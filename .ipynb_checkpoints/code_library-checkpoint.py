e43e95b8a63cfb85aa60bba2a7304ba65b99461a
EC2 Default User <ec2-user@ip-172-31-11-186.ec2.internal>
2024-06-15 16:06:28 +0000

    created a function which can copy csv files from s3 to a snowflake table

--- .ipynb_checkpoints/code_library-checkpoint.py
+++ .ipynb_checkpoints/code_library-checkpoint.py
@@ -4,6 +4,17 @@ import pandas as pd
 from dotenv import dotenv_values
 secrets = dotenv_values(".env")
 
+connection_parameters = {
+"account":secrets['account'], 
+"user":secrets['user'], 
+"password": secrets['password'], 
+"role":secrets['role'], 
+"warehouse":"COMPUTE_WH", 
+"database":"DEMO_DB", 
+"schema":"PUBLIC" 
+}
+
+
 def snowconnection(connection_config):
     session = Session.builder.configs(connection_config).create()
     session_details = session.create_dataframe(
@@ -19,12 +30,27 @@ def snowconnection(connection_config):
     session_details.write.mode("append").save_as_table("session_audit")
     return session
     
-connection_parameters = {
-"account":secrets['account'], 
-"user":secrets['user'], 
-"password": secrets['password'], 
-"role":secrets['role'], 
-"warehouse":"COMPUTE_WH", 
-"database":"DEMO_DB", 
-"schema":"PUBLIC" 
-}
+
+def copy_to_table(session,config_file,schema='NA'):
+    database_name = config_file.get("Database_name")
+    Schema_name = config_file.get("Schema_name")
+    Target_table = config_file.get("Target_table")
+    target_columns = config_file.get("target_columns")
+    on_error = config_file.get("on_error")
+    Source_location = config_file.get("Source_location")
+
+
+    if config_file.get("Source_file_type") == 'csv':
+            schema = schema
+            df = session.read.options({"skip_header":1}).schema(schema).csv("'"+Source_location+"'")
+    else :
+        raise "The program currently only supports csv source_file_type"
+        
+    with session.query_history() as query_history:
+        copied_into_result = df.copy_into_table(database_name+"."+Schema_name+"."+Target_table, target_columns=target_columns,force=True,on_error=on_error )
+    query = query_history.queries
+    # Mention command to collect query id of copy command executed.
+    for id in query:
+        if "COPY" in id.sql_text:
+            qid = id.query_id
+    return copied_into_result, qid
