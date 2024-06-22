6f5f11ec05dfee0832da6e40080af7444b617d9b
EC2 Default User <ec2-user@ip-172-31-11-186.ec2.internal>
2024-06-22 15:12:56 +0000

    added a funciton to copy semi-structured data, avro files from s3 to snowflake

--- code_library.py
+++ code_library.py
@@ -71,12 +71,12 @@ def copy_to_table(session,config_file,schema='NA'):
 
 
 def collect_rejects(session,qid,config_file):
-        """
-        # def collect_rejects(session,qid,config_file):
-            > https://docs.snowflake.com/en/sql-reference/functions/validate
-            > Collect all the errors which happned while trying to load using a copy commmand (quiery id of the copy command is there in the qid parameter
-            >
-        """
+    """
+    # def collect_rejects(session,qid,config_file):
+        > https://docs.snowflake.com/en/sql-reference/functions/validate
+        > Collect all the errors which happned while trying to load using a copy commmand (quiery id of the copy command is there in the qid parameter
+        >
+    """
     database_name = config_file.get("Database_name")
     Schema_name = config_file.get("Schema_name")
     Target_table = config_file.get("Target_table")
@@ -84,3 +84,90 @@ def collect_rejects(session,qid,config_file):
     rejects = session.sql("select *  from table(validate("+database_name+"."+Schema_name+"."+Target_table+" , job_id =>"+ "'"+ qid +"'))")
     rejects.write.mode("append").save_as_table(Reject_table)
     return rejects
+    
+def map_columns(df,map_columns):
+    """
+     Remove double qoutes from the column names and drop unwanted columns
+     Rename the dataframe column names, this is necessary because after renaming the columns we put this dataframe into an internal stage as a csv file, from where we use the copy command to load the data into the snowflake table so that we can get the on_error continue option.
+    """
+    # Remove double qoutes from the column names and drop unwanted columns
+    cols = df.columns
+    map_keys = [key.upper() for key in map_columns.keys()]
+    for c in cols:
+        df = df.with_column_renamed(c,c.replace('"',''))
+    cols = df.columns
+    for c in cols:
+        if c.upper() not in map_keys:
+            print("Dropped column,"+" "+c.upper())
+            df=df.drop(c.upper())   
+
+    # Rename the dataframe column names
+    for k,v in map_columns.items():
+        df = df.with_column_renamed(k.upper(),v.upper())
+    return df
+    
+def copy_to_table_semi_struct_data(session,config_file,schema='NA'):
+    
+    """
+    This function will help in copying avro data from s3 to snowflake
+    In the config_file we can specify the following details
+    {
+    "Database_name":"DEMO_DB",
+    "Schema_name":"PUBLIC",
+    "Target_table":"EMP_DETAILS_AVRO_CLS",
+    "Reject_table":"EMPLOYEE_AVRO_REJECTS",      
+    "target_columns":["REGISTRATION",
+    "USER_ID",
+    "FIRST_NAME",
+    "LAST_NAME",
+    "USER_EMAIL"],
+    "on_error":"CONTINUE",
+    "Source_location":"@DEMO_DB.EXTERNAL_STAGES.S3_STG2/sp_avro/",
+    "Source_file_type":"avro",
+    "map_columns":{
+        "REGISTRATION_DTTM":"REGISTRATION",
+        "ID":"USER_ID",
+        "FIRST_NAME":"FIRST_NAME",
+        "LAST_NAME":"LAST_NAME",
+        "EMAIL":"USER_EMAIL"
+    }
+    
+    }
+    The target_columns specify the columns in the target table present inside snowflake (it is not compulosry for the table to exist, if it dosent exist the framework will create the table)
+    The map_columns key contains a dictionary, the keys in the dictionary shows the column name in the source avro file which is currently in the s3 bucket and the values shows the corresponding column names in the table which is in snowflake
+    
+    """
+    database_name = config_file.get("Database_name")
+    Schema_name = config_file.get("Schema_name")
+    Target_table = config_file.get("Target_table")
+    target_columns = config_file.get("target_columns")
+    on_error = config_file.get("on_error")
+    Source_location = config_file.get("Source_location")
+    transformations = config_file.get("transformations")
+    maped_columns = config_file.get("map_columns")
+    # print(f"maped_columns : {maped_columns}")
+
+    if config_file.get("Source_file_type") == 'csv':
+            return "Expecting semi structured data but got csv"
+    elif config_file.get("Source_file_type") == 'avro':
+        df = session.read.avro(Source_location)
+    
+    # Map columns in df to target table
+    df = map_columns(df,maped_columns)
+
+    # Create temporary stage
+    _ = session.sql("create or replace temp stage demo_db.public.mystage").collect()
+    remote_file_path = '@demo_db.public.mystage/'+Target_table+'/'
+    # Write df to temporary internal stage location
+    df.write.copy_into_location(remote_file_path, file_format_type="csv", format_type_options={"FIELD_OPTIONALLY_ENCLOSED_BY":'"'}, header=False, overwrite=True)
+    
+    # Read the file from temp stage location
+    df = session.read.schema(schema).csv("'"+remote_file_path+"'")
+    with session.query_history() as query_history:
+        copied_into_result = df.copy_into_table(database_name+"."+Schema_name+"."+Target_table, target_columns=target_columns,force=True,on_error=on_error,format_type_options={"FIELD_OPTIONALLY_ENCLOSED_BY":'"'})
+    query = query_history.queries
+    # Mention command to collect query id of copy command executed.
+    for id in query:
+        if "COPY" in id.sql_text:
+            qid = id.query_id
+    return copied_into_result, qid
--- schema/src_stg_schema.py
+++ schema/src_stg_schema.py
@@ -8,3 +8,12 @@ StructField("EMAIL", StringType()),
 StructField("ADDRESS", StringType()),
 StructField("CITY", StringType()),
 StructField("DOJ",DateType())])
+
+int_emp_details_avro = StructType([StructField('registration_dttm', StringType(16777216), nullable=False), StructField('id', LongType(), nullable=False), StructField('first_name', StringType(16777216), nullable=False), StructField('last_name', StringType(16777216), nullable=False), StructField('email', StringType(16777216), nullable=False), StructField('gender', StringType(16777216), nullable=False), StructField('ip_address', StringType(16777216), nullable=False), StructField('cc', LongType(), nullable=True), StructField('country', StringType(16777216), nullable=False), StructField('birthdate', StringType(16777216), nullable=False), StructField('salary', DoubleType(), nullable=True), StructField('title', StringType(16777216), nullable=False), StructField('comments', StringType(16777216), nullable=False)]) 
+
+
+emp_details_avro_cls = StructType([StructField('REGISTRATION', StringType(), nullable=False), 
+    StructField('USER_ID', LongType(), nullable=False), 
+        StructField('FIRST_NAME', StringType(), nullable=False), 
+            StructField('LAST_NAME', StringType(), nullable=False), 
+                StructField('USER_EMAIL', StringType(), nullable=False)])
